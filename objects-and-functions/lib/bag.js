987e66305c6c18ce2ba21b90e32333c975936516
SKG360 <samuel.giles@colorado.edu>
2020-05-09 08:55:24 -0600

    completes bag test

---
+++ objects-and-functions/lib/bag.js
@@ -0,0 +1,23 @@
+class Bag {
+  constructor () {
+    this.empty = 1;
+    this.count = 0;
+    this.candies = [];
+  }
+
+  fill(candy) {
+    this.candies.push(candy);
+    this.count += 1;
+  }
+
+  contains(candyType) {
+    if (candyType === this.candies[0].type) {
+      return true;
+    } else {
+      return false
+    };
+  }
+
+};
+
+module.exports = Bag;
